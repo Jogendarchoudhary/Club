4f699b2f5794f763d9aac3b9415e3a428295d19f
Jogendar Singh <josingh@blackberry.com>
2020-06-11 00:51:26 +0530

    Analysis: Need to commit Club project initial setup Cases:- - Initial project setup - Added Company list view controller - Added Member list view controller

---
+++ Club/ClubViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ClubViewController.swift
+//  Club
+//
+//  Created by AA/MP/05 on 11/06/20.
+//  Copyright © 2020 Jogendar Singh. All rights reserved.
+//
+
+import UIKit
+
+class ClubViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ Club/MembersViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MembersViewController.swift
+//  Club
+//
+//  Created by AA/MP/05 on 11/06/20.
+//  Copyright © 2020 Jogendar Singh. All rights reserved.
+//
+
+import UIKit
+
+class MembersViewController: ClubViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
